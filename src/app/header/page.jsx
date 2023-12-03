32c3c3c6ca9e5918987e0257144b6509bfb8bfc7
mrsayed2022 <am6776756@gmail.com>
2023-12-03 22:55:30 +0200

    first commit

--- src/app/header/page.jsx
+++ src/app/header/page.jsx
@@ -8,20 +8,20 @@ function HeaderD() {
 <nav>
 
     <div >
-<ul className="flex mt-2 justify-center sm:justify-center md:justify-end w-[100%] text-sm sm:text-lg font-medium sm:font-bold absolute z-10">
+<ul className="flex mt-2 font-bold justify-center sm:justify-center md:justify-end w-[100%] text-sm sm:text-lg font-medium sm:font-bold absolute z-10">
         
-    <li className='ml-2 sm:ml-6  cursor-pointer'><Link href="/">Home </Link></li>
+    <li className='ml-2 text-[small] sm:text-[large] sm:ml-6  cursor-pointer'><Link href="/">Home </Link></li>
        
 
 
-    <li className='ml-2 sm:ml-6 cursor-pointer'><Link href="receve">Appointment</Link></li>
+    <li className='ml-2 text-[small] sm:text-[large] sm:ml-6 cursor-pointer'><Link href="receve">Appointment</Link></li>
 
    
     
-    <li className='ml-2 sm:ml-6 cursor-pointer'><Link href="/services">Service </Link></li>
+    <li className='ml-2 text-[small] sm:text-[large] sm:ml-6 cursor-pointer'><Link href="/services">Service </Link></li>
    
    
-    <li className='ml-2 sm:ml-6 mr-2 cursor-pointer'><Link href="/appoint">About</Link></li>
+    <li className='ml-2 text-[small] sm:text-[large] sm:ml-6 mr-2 cursor-pointer'><Link href="/appoint">About</Link></li>
    
 </ul>
     </div>
@@ -41,11 +41,11 @@ export function HeaderO() {
 <ul className="flex mt-2 justify-start sm:justify-start md:justify-start w-[100%] text-sm sm:text-lg font-medium sm:font-bold text-white  absolute z-10">
 
 
-    <li className='ml-6   cursor-pointer'><Link href="blog">Review</Link></li>
+    <li className='ml-6 text-[small] sm:text-[large]  cursor-pointer'><Link href="blog">Review</Link></li>
 
    
 
-    <li className='ml-6   cursor-pointer'> <Link href="contact">Contact Us</Link></li>
+    <li className='ml-6 text-[small] sm:text-[large]  cursor-pointer'> <Link href="contact">Contact</Link></li>
     
 </ul>
     </div>
